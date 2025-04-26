699e431b00d3b840ae83b472ecf48ad460d5213d
kshitij-parad <kshitijparad964@gmail.com>
2025-04-26 18:30:24 +0530

    crated userDetailService with pass and {noop} and Bencrypter

--- src/main/java/com/spring/security/config/WebSecurityConfig.java
+++ src/main/java/com/spring/security/config/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -26,4 +32,29 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails kshitij = User.withUsername("kshitij")
+                                    .password("{noop}kp")
+                                    .roles("USER")
+                                    .build();
+
+        UserDetails raj = User.withUsername("raj")
+                                    .password(passwordEncoder().encode("raj"))
+                                    .roles("USER")
+                                    .build();
+        
+        return new InMemoryUserDetailsManager(kshitij,raj);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
